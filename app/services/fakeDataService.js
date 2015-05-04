caabac2a7a10291fadb575bcee95a3e664a7f7be
Arnold Krumins <arnold.krumins@gmail.com>
2015-05-04 10:26:07 +0200

    Adding new RTB Buyers

--- app/services/fakeDataService.js
+++ app/services/fakeDataService.js
@@ -7,14 +7,20 @@ app.factory('fakeDataService',function(){
 
         getComponents: function(){
               return [
-                  {name:'Advertiser One', settingone:'100',settingtwo:'80',settingthree:'80',settingfour:'90'},
-                  {name:'Advertiser Two', settingone:'35',settingtwo:'60',settingthree:'90',settingfour:'86'},
-                  {name:'Advertiser Three', settingone:'80',settingtwo:'70',settingthree:'100',settingfour:'35'},
-                  {name:'Advertiser Four', settingone:'86',settingtwo:'86',settingthree:'80',settingfour:'77'},
-                  {name:'Advertiser Five', settingone:'55',settingtwo:'78',settingthree:'78',settingfour:'37'},
-                  {name:'Advertiser Six', settingone:'22',settingtwo:'66',settingthree:'44',settingfour:'97'},
-                  {name:'Advertiser Seven', settingone:'88',settingtwo:'27',settingthree:'47',settingfour:'25'},
-                  {name:'Advertiser Eight', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'}
+                  {name:'Turn', settingone:'100',settingtwo:'80',settingthree:'80',settingfour:'90'},
+                  {name:'Sociomantic', settingone:'35',settingtwo:'60',settingthree:'90',settingfour:'86'},
+                  {name:'Digilant', settingone:'80',settingtwo:'70',settingthree:'100',settingfour:'35'},
+                  {name:'Delta Projects', settingone:'86',settingtwo:'86',settingthree:'80',settingfour:'77'},
+                  {name:'BidTheatre', settingone:'55',settingtwo:'78',settingthree:'78',settingfour:'37'},
+                  {name:'Appnexus', settingone:'22',settingtwo:'66',settingthree:'44',settingfour:'97'},
+                  {name:'Adform', settingone:'88',settingtwo:'27',settingthree:'47',settingfour:'25'},
+                  {name:'BidSwitch', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'MediaMath', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'Index (by Casale Media)', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'BidSwitch US', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'MediaMath US', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'DoubleClick Bid Manager US', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'},
+                  {name:'BidTheatre - Blind', settingone:'54',settingtwo:'89',settingthree:'79',settingfour:'56'}
               ];
         }
 
