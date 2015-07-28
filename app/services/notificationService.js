efc724c3d24766b234ddc0190c7cbdbe5b3eda5a
Arnold Krumins <arnold.krumins@gmail.com>
2015-07-28 18:30:52 +0200

    changing spacing and signature of service

--- app/services/notificationService.js
+++ app/services/notificationService.js
@@ -9,25 +9,16 @@ app.factory('notificationService', ['toastr', function(toastr){
     return{
 
         success : function(title,message){
-
-
             toastr.success(message, title,options);
-
         },
         info: function(title,message){
-
             toastr.info(message, title,options);
-
         },
         warning: function(title,message){
-
             toastr.warning(message, title,options);
-
         },
-        Error: function(title,message){
-
+        error: function(title,message){
             toastr.error(message, title,options);
-
         }
 
     }
