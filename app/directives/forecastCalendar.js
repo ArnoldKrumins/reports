df9cda76ff7bb22aa6674c42646bea37632e46c6
Arnold Krumins <arnold.krumins@gmail.com>
2015-09-13 20:36:39 +0200

    adding indicator classes and logic

--- app/directives/forecastCalendar.js
+++ app/directives/forecastCalendar.js
@@ -3,7 +3,6 @@
  */
 app.directive('forecastCalendar',function(){
 
-
     return{
         resrict:'EA',
         replace:true,
@@ -16,9 +15,9 @@ app.directive('forecastCalendar',function(){
                             '<div class="f-data">' +
                                '<p>{{ formatNumber(forecast.avails).toString().concat(\'&nbsp;K\') }}</p>' +
                                 '<p>{{ formatNumber(forecast.sold).toString().concat(\'&nbsp;K\') }}</p>' +
-                                '<p>{{ forecast.revenue | currency }}</p>' +
+                                '<p>{{ forecast.revenue | currency }}&nbsp;{{ \'(\'.concat(status(forecast.avails,forecast.sold),\'%\',\')\') }}</p>' +
                             '</div>' +
-                            '<div class="f-indicator"></div>' +
+                            '<div class="f-indicator" ng-class="{\'good\' : status(forecast.avails,forecast.sold) >= 40,\'ok\' : status(forecast.avails,forecast.sold) <40,\'bad\' : status(forecast.avails,forecast.sold) <20 }"></div>' +
                             '</div>' +
                         '</li>' +
                     '</ul>',
@@ -28,10 +27,17 @@ app.directive('forecastCalendar',function(){
                 return parseInt(value);
             }
 
+            scope.status = function(avails,sold){
+                return parseInt(((parseInt(sold) / parseInt(avails)) * 100));
+            }
 
+            scope.statusClass = function(avails,sold){
+                var val = parseInt(((parseInt(sold) / parseInt(avails)) * 100));
+
+                if(val >=40) return true;
+                if(val <40) return false;
+            }
         }
-
-
 
 
 
